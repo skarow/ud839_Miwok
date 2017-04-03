a48cbfd1e1f18f4afa9e9c8b026c04c4342f4235
Kalin Pezhgorski <kalin.pezhgorski@egt-bg.com>
2017-04-03 14:15:49 +0300

    added ArrayList of number names and ArrayAddapter for displaying the list and recycle text views

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -18,9 +18,13 @@ package com.example.android.miwok;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.concurrent.Callable;
 
 public class MainActivity extends AppCompatActivity {
 
